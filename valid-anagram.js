d50d22baf1da4198ab81f57010afbe79184a5ac6
renesanger <renejsanger@gmail.com>
2023-08-21 23:31:49 -0400

    Update and rename is-anagram.js to valid-anagram.js

--- valid-anagram.js
+++ valid-anagram.js
@@ -1,7 +1,11 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+/*
+    242. Valid Anagram
+    Level: easy
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+    Example 1:
+    
+    Input: s = "anagram", t = "nagaram"
+    Output: true
  */
 var isAnagram = function(s, t) {
     if(s.length != t.length)
