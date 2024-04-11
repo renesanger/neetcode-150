ce2c41ad5a51333f06c19087346b1ac49b20e606
renesanger <renejsanger@gmail.com>
2024-04-11 12:43:42 -0400

    Create 0150-evaluate-reverse-polish-notation.cs

---
+++ 0150-evaluate-reverse-polish-notation.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    private static int evaluate(int b, int a, string op) => op switch{
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        "/" => a / b,
+        _   => throw new Exception()
+    };
+    public int EvalRPN(string[] tokens) {
+        Stack<int> stack = new Stack<int>();
+        int result = 0;
+        foreach(var token in tokens){
+            int number = 0;
+            var isNumber = int.TryParse(token, out number);
+            if(isNumber){
+                stack.Push(number);
+            }
+            else {
+                result = evaluate(stack.Pop(), stack.Pop(), token);
+                stack.Push(result);
+            }
+        }
+
+        return stack.Pop();
+    }
+}
