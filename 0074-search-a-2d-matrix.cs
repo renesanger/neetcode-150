dad9db2e13fb333c43145c473bfcd81e22cddb03
renesanger <renejsanger@gmail.com>
2024-04-15 20:33:04 -0400

    Update 0074-search-a-2d-matrix.cs

--- 0074-search-a-2d-matrix.cs
+++ 0074-search-a-2d-matrix.cs
@@ -2,25 +2,40 @@ public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
         int ROWS = matrix.Length;
         int COLS = matrix[0].Length;
-        int top = 0, bot = ROWS - 1;
+
+        int top = 0;
+        int bot = ROWS - 1;
         int row = 0;
 
-        while(top <= bot) {
+        while(top <= bot){
             row = (top + bot) / 2;
-            if(target > matrix[row][COLS-1]) top = row + 1;
-            else if (target < matrix[row][0]) bot = row - 1;
-            else break;
+            if(target < matrix[row][0]){
+                bot = row - 1;
+            }
+            else if(target > matrix[row][COLS - 1]){
+                top = row + 1;
+            }
+            else{
+                break;
+            }
         }
 
-        if (top > bot) return false;
+        if(top > bot)
+            return false;
 
-        int l = 0, r = COLS - 1;
-
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (target > matrix[row][m]) l = m + 1;
-            else if (target < matrix[row][m]) r = m - 1;
-            else return true;
+        row = (top + bot) / 2;
+        int l = 0; int r = COLS - 1;
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(target > matrix[row][mid]){
+                l = mid + 1;
+            }
+            else if(target < matrix[row][mid]){
+                r = mid - 1;
+            }
+            else{
+                return true;
+            }
         }
 
         return false;
